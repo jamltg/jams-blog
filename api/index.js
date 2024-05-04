9dce3b683983a5d43617e881a1cd2695c3609775
jamltg <jamletigio@gmail.com>
2024-05-04 11:32:39 +0800

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 //take note that all installations are from root folder and not in api directory
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+//added a collection name (jams-blog) which is written after --oxy7hd2.mongodb.net/--
+mongoose.connect(process.env.MONGO)
+.then(()=>{console.log('MongoDB is connected')}).catch((err)=>{console.log(err)})
 
 const app = express();
 
