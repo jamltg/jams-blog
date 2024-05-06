a16e5463a730c021ab6e51a4c87a8f364287806d
jamltg <jamletigio@gmail.com>
2024-05-06 18:49:30 +0800

    add redux-persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit';
+/* eslint-disable no-undef */
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice.js';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+
+//installed redux-persist so data wont disappear when page is reloaded 
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+//keyword root goes to localstorage so it wont disappear in reload
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-})
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({serializableCheck:false}),
+});
+
+export const persistor = persistStore(store);
